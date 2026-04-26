ec66dd6c9df7a1ef46ad806970bae0cbd2846081
SteveWill-iam <if-24017@students.ithb.ac.id>
2026-04-26 16:03:49 +0700

    ubah nama socialgram, bg login/regist

--- Connection/Connection.php
+++ Connection/Connection.php
@@ -2,7 +2,7 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $database = "db_ogram";
+    $database = "db_socialgram";
 
     $connection = mysqli_connect($servername,$username,$password,$database);
 ?>
