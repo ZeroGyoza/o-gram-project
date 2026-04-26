bec16d41a36b53802e0a41475fa81d5d7cace713
SteveWill-iam <if-24017@students.ithb.ac.id>
2026-04-26 16:22:20 +0700

    undo

--- Connection/Connection.php
+++ Connection/Connection.php
@@ -2,7 +2,7 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $database = "db_socialgram";
+    $database = "db_ogram";
 
     $connection = mysqli_connect($servername,$username,$password,$database);
 ?>
